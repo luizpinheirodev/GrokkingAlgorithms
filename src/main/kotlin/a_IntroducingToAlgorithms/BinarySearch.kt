7f3a27fa5d8dcc313403a94839a79e965c68724e
luiz_geraldo <luiz_geraldo@sicredi.com.br>
2023-03-25 11:05:44 -0300

    Create project and introducing book

---
+++ src/main/kotlin/a_IntroducingToAlgorithms/BinarySearch.kt
@@ -0,0 +1,21 @@
+package a_IntroducingToAlgorithms
+
+fun List<Int>.binarySearch(item: Int) : Int? {
+    var min = 0
+    var max = lastIndex
+    while (min <= max) {
+        val mid = (min + max)/2
+        val guess = this[mid]
+        if (guess == item) return mid
+        if (guess > item) max = mid -1
+        else min = mid + 1
+    }
+    return null
+}
+
+val sortedArray = (0..100 step 3).toList()
+
+fun main() {
+    println(2*2*2*2*2*2*2*2)
+    print(sortedArray.binarySearch(6))
+}
